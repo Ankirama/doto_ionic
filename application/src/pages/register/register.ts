376cd43669907a465c44f60b5c4defce8c2d2ae3
Ankirama <fabien.martinez@epitech.eu>
2017-10-15 01:59:35 +0200

    [Register]: Create basic register validator

--- application/src/pages/register/register.ts
+++ application/src/pages/register/register.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { Validators, FormBuilder, FormGroup} from '@angular/forms';
 
+import { PasswordValidator } from '../../validators/password';
+
 /**
  * Generated class for the RegisterPage page.
  *
@@ -20,6 +22,25 @@ export class RegisterPage {
 
   constructor(public navCtrl: NavController, public navParams: NavParams, public formBuilder: FormBuilder) {
     this.navCtrl = navCtrl;
+
+    this.authForm = formBuilder.group({
+      email: ['', Validators.compose([Validators.required, Validators.pattern('^[_a-z0-9]+(\.[_a-z0-9]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,24})$')])],
+      password: ['', Validators.compose([Validators.required, Validators.minLength(8)])],
+      password_check: ['', Validators.compose([Validators.required, Validators.minLength(8)])]
+    }, {'validator': PasswordValidator.isMatching});
+  }
+
+  onSubmit(value: any): void {
+    if(this.authForm.valid) {
+      window.localStorage.setItem('email', value.email.toLowerCase());
+      window.localStorage.setItem('password', value.password);
+      location.reload();
+    }
+  }
+
+  ionViewDidEnter() {
+    console.log('reset form');
+    this.authForm.reset();
   }
 
   ionViewDidLoad() {

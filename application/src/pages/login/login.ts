46507ddd8037fea4df86d1ed1fc6382318fb7037
Ankirama <fabien.martinez@epitech.eu>
2017-10-15 16:04:21 +0200

    [Login]: Add login feature

--- application/src/pages/login/login.ts
+++ application/src/pages/login/login.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
+import { Validators, FormBuilder, FormGroup} from '@angular/forms';
+import { AuthProvider } from '../../providers/auth/auth';
 /**
  * Generated class for the LoginPage page.
  *
@@ -15,7 +16,40 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class LoginPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  authForm: FormGroup;
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, public formBuilder: FormBuilder, public auth: AuthProvider) {
+    this.navCtrl = navCtrl;
+    this.authForm = formBuilder.group({
+      email: ['', Validators.compose([Validators.required, Validators.pattern('^[_a-z0-9]+(\.[_a-z0-9]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,24})$')])],
+      password: ['', Validators.compose([Validators.required])],
+    });
+  }
+
+  onSubmit(value: any): void {
+    if(this.authForm.valid) {
+      let credentials = {
+        email: value.email.toLowerCase(),
+        password: value.password
+      };
+      this.auth.loginWithEmail(credentials)
+        .then(data => {
+          console.log('data ==> ', data);
+          location.reload();
+        })
+        .catch(error => {
+          if (error.code === "auth/invalid-email" || error.code === "auth/wrong-password") {
+            alert("Your combination email / password is not good.");
+          } else {
+            alert(error.message);
+          }
+        });
+    }
+  }
+
+  ionViewDidEnter() {
+    console.log('reset form');
+    this.authForm.reset();
   }
 
   ionViewDidLoad() {

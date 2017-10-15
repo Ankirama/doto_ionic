9753ab848d3415809da08bf22171c87357bc49bd
Ankirama <fabien.martinez@epitech.eu>
2017-10-15 03:05:22 +0200

    Remove auth feature: not yet created

--- application/src/app/app.component.ts
+++ application/src/app/app.component.ts
@@ -3,7 +3,6 @@ import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
-import { AuthPage } from '../pages/auth/auth';
 import { TabsPage } from '../pages/tabs/tabs';
 
 @Component({
--- application/src/app/app.module.ts
+++ application/src/app/app.module.ts
@@ -3,13 +3,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 
-import { LoginPage } from '../pages/login/login';
-import { RegisterPage } from '../pages/register/register';
-
 import { AboutPage } from '../pages/about/about';
 import { ContactPage } from '../pages/contact/contact';
 import { HomePage } from '../pages/home/home';
-import { AuthPage } from '../pages/auth/auth';
 import { TabsPage } from '../pages/tabs/tabs';
 
 import { StatusBar } from '@ionic-native/status-bar';
@@ -18,9 +14,6 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 @NgModule({
   declarations: [
     MyApp,
-    AuthPage,
-    LoginPage,
-    RegisterPage,
     AboutPage,
     ContactPage,
     HomePage,
@@ -33,9 +26,6 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    AuthPage,
-    LoginPage,
-    RegisterPage,
     AboutPage,
     ContactPage,
     HomePage,
